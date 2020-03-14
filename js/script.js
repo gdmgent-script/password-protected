6433f590fdd81c955bff604e17fddd372d52074d
hellothrive <hello@thrive.be>
2020-03-14 17:12:01 +0100

    wip

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ let counter = 0;
 let promptText = 'Give password';
 
 do{
-
     givenPassword = prompt(promptText);
     promptText = 'Wrong, give password again.'
     counter++;
 }while(givenPassword != secretPassWord)
 
 document.write(`Welkom, jij weet het wachtwoord! Je hebt ${counter}x proberen raden`);
+
+//MEERDERE OPLOSSINGEN MOGELIJKE
