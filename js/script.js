0ba79e5206d443a8c2f5640690852359cfc1f292
hellothrive <hello@thrive.be>
2020-03-14 17:11:45 +0100

    wip

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-// 1. Create variable with secret password. Create an other variable with a counter, starting from 0
 
+const secretPassWord = "awesomeShit";
+let givenPassword;
+let counter = 0;
+let promptText = 'Give password';
 
-// 2. Ask for the secret password with a prompt
+do{
 
-// 3. Check if the secret password is the same as the given password in the prompt
-    // If it is the same document.write : ‘Welkom, jij weet het wachtwoord! Je hebt 7x proberen raden`
+    givenPassword = prompt(promptText);
+    promptText = 'Wrong, give password again.'
+    counter++;
+}while(givenPassword != secretPassWord)
 
-    // If wrong: Add one to the counter and show the prompt again
-
-
+document.write(`Welkom, jij weet het wachtwoord! Je hebt ${counter}x proberen raden`);
