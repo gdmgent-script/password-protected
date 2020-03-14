95eba7b9ba0230b92333da278e6ae776da87786b
hellothrive <hello@thrive.be>
2020-03-14 16:58:32 +0100

    wip

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// 1. Create variable with secret password. Create an other variable with a counter, starting from 0
+
+
+// 2. Ask for the secret password with a prompt
+
+// 3. Check if the secret password is the same as the given password in the prompt
+    // If it is the same document.write : ‘Welkom, jij weet het wachtwoord! Je hebt 7x proberen raden`
+
+    // If wrong: Add one to the counter and show the prompt again
+
+
